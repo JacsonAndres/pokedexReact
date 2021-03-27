12a2df427e0c950519751effb1a062fb21e64aa0
jacson <jacsonburgos1@gmail.com>
2021-03-27 10:34:22 -0500

    branch

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./index.css";
 class Input extends React.Component {
-  render(props) {
+  state = {};
+  render() {
     const { label } = this.props;
-
+    const onChange = (evento) => {
+      this.setState({
+        texto: evento.target.value,
+      });
+    };
+    const onClick = () => {
+      console.log(this.state);
+    };
     return (
       <div>
         <label>{label}</label>
         <br />
-        <input class="input " type="text" placeholder={label} />
+        <div>{this.state.texto}</div>
+        <br />
+        <input
+          onChange={onChange}
+          class="input "
+          type="text"
+          placeholder={label}
+        />
+
+        <button onClick={onClick}> logeame</button>
       </div>
     );
   }
